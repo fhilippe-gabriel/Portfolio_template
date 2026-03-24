b90f9b2fd9f07974d32203ea75eee46863fffeac
Fhilippe Gabriel <fhilippedev@gmail.com>
2026-03-23 21:42:38 -0300

    refactor: update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('public.layout');
-});
+// Route::get('/', function () {
+//     return view('public.layout');
+// });
+
+Route::get('/', [PortfolioController::class, 'index'])->name('home');
